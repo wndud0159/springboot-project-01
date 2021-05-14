b1ce04ac4e71ad9cac8ba416824d113acb895697
wndud0159 <wndud0159@naver.com>
2021-05-14 14:56:36 +0900

    Controller Testing

---
+++ src/main/java/com/wndud0159/springboot/SpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wndud0159.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wndud0159/springboot/SpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wndud0159.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
